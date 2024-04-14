fc046151f95bd73454bd3d7a8f769e407a8d0448
Flavio Oria <flavio.oriap@gmail.com>
2024-04-14 23:23:12 +0200

    ✨ Update styles and items in header default

--- components/HeaderDefault.vue
+++ components/HeaderDefault.vue
@@ -2,11 +2,12 @@
 
 <template>
   <header
-    class="flex items-center justify-around p-3 bg-white h-[80px] fixed w-full top-0 left-0 right-0"
+    class="w-full h-[80px] fixed top-0 left-0 right-0 z-50 flex items-center md:justify-around p-3 bg-white"
   >
     <h1 class="font-title text-3xl cursor-pointer">Lau's Coffee</h1>
-    <div class="w-[60%]">
+    <div class="w-[60%] hidden md:block">
       <ul class="flex items-center justify-center flex-1 gap-10">
+        <li class="cursor-pointer hover:text-background_first">Inicio</li>
         <li class="cursor-pointer hover:text-background_first">Productos</li>
         <li class="cursor-pointer hover:text-background_first">
           Ofertas especiales
@@ -16,7 +17,7 @@
         </li>
       </ul>
     </div>
-    <div class="flex gap-3">
+    <div class="hidden md:flex gap-3">
       <Button
         label="Iniciar Sesión"
         class="bg-button_background_first text-button_text_first border-0"
