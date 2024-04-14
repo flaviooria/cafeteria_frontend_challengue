443f809dec364a201390c8d863d7beb8f1f2ba15
Flavio Oria <flavio.oriap@gmail.com>
2024-04-14 18:16:01 +0200

    ✨ Add primevue module and init setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss"]
-})
+  modules: ["@nuxtjs/tailwindcss", "nuxt-primevue"],
+
+  primevue: {
+    usePrimeVue: true,
+    components: {
+      include: "*",
+    },
+    cssLayerOrder: "tailwind-base, primevue, tailwind-utilities",
+  },
+});
