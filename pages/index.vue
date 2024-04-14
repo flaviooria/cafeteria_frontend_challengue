c902ec4752d067f57576630a61c16b944a64ecc2
Flavio Oria <flavio.oriap@gmail.com>
2024-04-14 18:23:33 +0200

    ✨ Changue view of index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,9 @@
 <template>
   <div>
-    
+    <h1>Hola Mundo</h1>
   </div>
 </template>
 
-<script lang="ts" setup>
+<script lang="ts" setup></script>
 
-</script>
-
-<style>
-
-</style>
+<style></style>
